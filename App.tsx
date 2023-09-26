1a66662cc38e1dd17db612c26b2cde84327b6073
JPablix <j.granados@estudiantec.cr>
2023-09-26 02:59:30 -0600

    FIX: App irregularity

--- App.tsx
+++ App.tsx
@@ -7,11 +7,8 @@ import { AuthProvider } from './src/context/AuthContext';
 
 // Fonts
 import { useFonts } from "@expo-google-fonts/oswald";
-<<<<<<< HEAD
+
 import { Fonts } from './src/constants/Fonts';
-=======
-import { Fonts } from './src/constants/fonts';
->>>>>>> aa594914b6f0e8ffb545884f62d66a44cae827ba
 
 // Navigation
 import { NavigationContainer } from '@react-navigation/native';
